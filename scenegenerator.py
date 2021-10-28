b026a3a621f4ccaadcb3af1d000587edf637c68b
haksulabs <tero.hakala@aalto.fi>
2021-10-28 16:23:15 +0300

    single ball scenes and a generator

---
+++ scenegenerator.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 28 15:42:01 2021
+
+@author: t
+"""
+
+import csv
+from random import random, choice
+
+
+def writelog(*args):
+    with open(filename, 'a+', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                         quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(args)
+     
+        
+filename = 'singles.csv'
+open(filename, 'w').close()   #empty file
+
+
+time = 500  #  time start in ms
+vx0=1
+
+scale = 800  
+
+for i in range(100):
+    time = time + (scale / abs(vx0))  # time when previous ball crosses center
+    x0 = choice([-1,1])  
+    y0 = choice([-1,1])*random()    
+    vx0 = -x0 * (random()/2 + 0.1)
+    vy0 = 0
+    writelog(time,x0,y0,vx0,vy0)
+    
+    
